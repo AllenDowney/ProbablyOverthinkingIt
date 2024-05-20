7fd0c4be0e53f533af11c6545010635a4c2a1a0e
Allen Downey <downey@allendowney.com>
2024-05-20 10:06:06 -0400

    Adding bertrand.ipynb

--- jb/build.sh
+++ jb/build.sh
@@ -12,7 +12,14 @@ cp ../notebooks/nbue.ipynb .
 cp ../notebooks/berkson.ipynb .
 cp ../notebooks/birthweight.ipynb .
 cp ../notebooks/longtail.ipynb .
+cp ../notebooks/base_rate.ipynb .
+cp ../notebooks/simpson.ipynb .
+cp ../notebooks/progress.ipynb .
+cp ../notebooks/overton.ipynb .
 cp ../notebooks/overton_irt.ipynb .
+cp ../examples/ansur_pca.ipynb .
+cp ../examples/bertrand.ipynb .
+cp ../examples/ideology_gap.ipynb .
 
 # build the HTML version
 jb build .
